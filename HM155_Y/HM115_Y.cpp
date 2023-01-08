666af83b3f365bb7a801c6a5ca900751ec426fd3
Ivan Ivanov <markelo495@gmail.com>
2023-01-09 02:45:23 +0300

    indian code

--- HM155_Y/HM115_Y.cpp
+++ HM155_Y/HM115_Y.cpp
@@ -76,15 +76,28 @@ void task1()
 }
 // не буду изменять, кину на проверку в коммит
 
-void task2()
+void task2() // нихуя не работает 9 задание на 155 странице
 {
-	double a = 0, min = 0, max = 0;  // min = numeric_limits<double>::max(), max = numeric_limits<double>::min();
+	double a = 0, min = 0, max = 0, summ = 0;  // min = numeric_limits<double>::max(), max = numeric_limits<double>::min();
 	int i = 0; //счётчик
 	string unit1 = "";
 
 	while (cin >> a >> unit1)
 	{
-		if (i == 0)
+		if (unit1 == "cm")
+		{
+			a /= 100;
+		}
+		else if (unit1 == "ft")
+		{
+			a = (a * 12) * 2.54;
+		}
+		else if (unit1 == "in")
+		{
+			a *= 2.54;
+		}
+
+		if (i == 0 and unit1 == "cm" or i == 0 and unit1 == "m" or i == 0 and unit1 == "ft" or i == 0 and unit1 == "in")
 		{
 			max = a;
 			cout << "Наибольшее среди ранее введённых " << a << unit1 << endl;
@@ -93,19 +106,24 @@ void task2()
 			++i;
 		}
 
-		if (a >= max and i != 1)
+		if (a >= max and i != 1 and unit1 == "cm" or a <= min and i != 1 and unit1 == "m" or a <= min
+			and i != 1 and unit1 == "ft" or a <= min and i != 1 and unit1 == "in")
 		{
 			max = a;
 			cout << "Наибольшее среди ранее введённых " << a << unit1 << endl;
 		}
 
-		if (a <= min and i != 1)
+		if (a <= min and i != 1 and unit1 == "cm" or a <= min and i != 1 and unit1 == "m" or a <= min
+			and i != 1 and unit1 == "ft" or a <= min and i != 1 and unit1 == "in")
 		{
 			min = a;
 			cout << "Наименьшее среди ранее введённых " << min << unit1 << endl;
 		}
 		++i;
+		if (unit1 == "cm" or unit1 == "m" or unit1 == "ft" or unit1 == "in")
+			summ += a;
 	}
+	cout << summ;
 }
 
 std::unordered_map<std::string, std::function<void()>> TASKS_MAP =
@@ -121,6 +139,7 @@ int main()
 	setlocale(LC_ALL, "Rus");
 
 	std::string task_name;
+	std::cout << "введите номер таска : ";
 	std::cin >> task_name;
 	TASKS_MAP[task_name]();
 }
